d5c7aac15e4b75514f8136b8f9e84720837b3317
Neommastermind <31413602+Neommastermind@users.noreply.github.com>
2018-05-13 01:17:00 -0600

    Finished
    The finished version.

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	//A reference to the player object
+	public GameObject player;
+	//The offset between the player and the camera
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame
+	//Guaranteed to run after all items have been processed in update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
-	//The Game objects RigidyBody component
+	//The speed of the ball
+	public float speed;
+	//The text for the count variable
+	public Text countText;
+	//The win text
+	public Text winText;
+	//A reference to the players rigid body component
 	private Rigidbody rb;
+	//The score count
+	private int count;
 
-	//Runs when the scene starts
+	//Runs on the first frame that the script is active
 	void Start() {
 		//Gets the rigid body component
 		rb = GetComponent<Rigidbody>();
+		//Initilize the players count and count text
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	//Called Before Rendering a frame
@@ -27,7 +40,40 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		//Apply the force vector to the game object
-		rb.AddForce (movement);
+		rb.AddForce (movement * speed);
 	}
 
+	//Defining how a trigger collision with the player works
+	void OnTriggerEnter(Collider other) {
+		//Check to see if the player hit a pick up object
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			//deactivate the pick ups upon collision, and increment the count
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		} /*else if (other.gameObject.CompareTag ("Pick Up Speed")) {
+			//Speed up the player
+			other.gameObject.SetActive (false);
+			speed += 10;
+		} else if (other.gameObject.CompareTag ("Pick Up Slow")) {
+			//Slow down the player
+			other.gameObject.SetActive (false);
+			speed -= 10;
+		} else if (other.gameObject.CompareTag ("Pick Up Grow")) {
+			//Grow the player
+			other.gameObject.SetActive (false);
+			transform.localScale += new Vector3(0.3f, 0, 0);
+		} else if (other.gameObject.CompareTag ("Pick Up Shrink")) {
+			//Shrink the player
+			other.gameObject.SetActive (false);
+			transform.localScale -= new Vector3(0.3f, 0, 0);
+		}*/
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 13) {
+			winText.text = "You Win!";
+		}
+	}
 }
